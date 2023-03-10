26434d543fbd3361e1f33344f16b56f987f9c1a5
Benni0706 <bennitallarek@gmail.com>
2023-03-10 13:14:38 +0100

    prisma setup

--- backend/routes/test.routes.ts
+++ backend/routes/test.routes.ts
@@ -1,12 +1,22 @@
 import { Application} from "express";
-const {getTests} = require("../database/database");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 module.exports = function (app: Application) {
 
-    app.get('/test', function (req, res) {
-        getTests(function(rows: any) {
-            res.send(rows);
-        })
+    app.get('/test/get', async (req, res) => {
+        let testdata = await prisma.test.findMany();
+        res.send(testdata);
+    });
+
+    app.get('/test/insert', async (req, res) => {
+        let test = await prisma.test.create({
+            data: {
+                testtext: 'Dies ist ein Test',
+            },
+        });
+        res.end();
     });
 
 }
