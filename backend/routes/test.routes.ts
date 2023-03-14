fe6785512ec96ccafcff8e85317129d7d289c95d
Benni0706 <61964978+Benni0706@users.noreply.github.com>
2023-03-14 11:47:16 +0100

    added prisma and sqlite db (#3)
    * added sqlite db and created module for communication with db
    
    * prisma setup
    
    * Update .gitignore
    
    * deleted test routes and test schema

--- backend/routes/test.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Application, Request, Response } from "express";
-
-module.exports = function (app: Application) {
-
-    app.get('/test', function (req, res) {
-        res.send('Guten Tag, API am H�rer. Wie kann ich Ihnen weiterhelfen?');
-    });
-
-}
