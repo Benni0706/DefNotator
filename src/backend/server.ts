d9b5661bc6ed505c0c13c095a80e7637f572716a
Tallarek, Benjamin <benjamin.tallarek@helmholtz-berlin.de>
2023-04-05 13:54:42 +0200

    cors setup and config

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -2,12 +2,28 @@ import { Application } from "express";
 
 const express = require('express');
 const app: Application = express();
+const cors = require('cors');
 
 const port = 3000;
 const hostname = 'localhost';
 
+
+/*
+CORS (Cross-Origin-Resource-Sharing)
+is needed so that our Vue app can access the API even though it has a different source
+*/
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+    methods: '*',
+}
+app.use(cors(corsOptions));
+
+//routes are being imported from route files
 const user = require('./routes/user');
 
+//app "uses" routes with route prefix
 app.use('/users', user);
 
 app.listen(port, hostname, () => {
