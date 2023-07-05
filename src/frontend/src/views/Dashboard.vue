624170ce689ec00419616cf0a5396113bc6da4c9
Tallarek, Benjamin <benjamin.tallarek@helmholtz-berlin.de>
2023-07-05 12:50:35 +0200

    datasets are now fetched from db and displayed

--- src/frontend/src/views/Dashboard.vue
+++ src/frontend/src/views/Dashboard.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+const datasets = ref<JSON>();
+
+async function getDatasets() {
+  try {
+    const api_url = import.meta.env.VITE_API_URL + '/users/datasets';
+    let data = await fetch(api_url, {
+      method: "GET",
+      credentials: "include"
+    });
+    if (data.ok) {
+      datasets.value = await data.json();
+    }
+  }
+  catch (err: any) {
+    console.log(err);
+  }
+}
+
+getDatasets();
 </script>
 
 <template>
   <main class="m-4">
     <h1 class="text-lg">Your datasets:</h1>
-    <div class="bg-gray-200 p-1 border-2 ">
-      datasets here
+    <div class="bg-gray-200 p-1 border-2">
+      <li v-for="dataset in datasets">
+        {{ dataset }}
+      </li>
     </div>
   </main>
 </template>
