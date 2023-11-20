665182c10e300a4433aa2d0b11694b8356618a78
roriwa <robinrwalter2002@gmail.com>
2023-11-20 22:52:22 +0100

    configured axios

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,8 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from "axios";
 
-import './assets/main.css'
+import './assets/main.css';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.withCredentials = true;
 
 const app = createApp(App)
 
