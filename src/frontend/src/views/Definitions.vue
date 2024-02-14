38b5f86549dac3d9052e433eb6e74b0a6abd034c
Benni0706 <bennitallarek@gmail.com>
2024-02-14 11:45:34 +0100

    syntax imporovements and parallel requests

--- src/frontend/src/views/Definitions.vue
+++ src/frontend/src/views/Definitions.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { reactive, computed, ref } from 'vue';
 import NavBar from '../components/NavBar.vue';
-import listElement from '../components/ListElement.vue';
+import ListElement from '../components/ListElement.vue';
 import { useRoute } from "vue-router";
 import axios, { HttpStatusCode } from 'axios';
 import type { CurrentUser } from '@/types/provided';
@@ -13,7 +13,7 @@ const route = useRoute();
 
 const datasetDefinitions = ref();
 const allDefinitions = ref();
-const tab = ref<Number>(0);
+const tab = ref<number>(0);
 const selectedDefinition = ref()
 
 const datasetName: string = route.params.datasetName.toString();
@@ -38,8 +38,10 @@ async function addNewDefinition() {
     console.log(response.data)
     const response_assign = await axios.post("/definitions/assign", { definitionId: response.data.id, datasetName: datasetName }, { validateStatus: null });
     if (response_assign.status == HttpStatusCode.Ok) {
-      getDatasetDefinitions()
-      getAllDefinitions()
+      await Promise.all([
+        getDatasetDefinitions(),
+        getAllDefinitions(),
+      ])
     }
   }
 }
@@ -47,16 +49,20 @@ async function addNewDefinition() {
 async function assignDefinition() {
   const response = await axios.post("/definitions/assign", { definitionId: selectedDefinition.value.id, datasetName: datasetName }, { validateStatus: null });
   if (response.status == HttpStatusCode.Ok) {
-    getDatasetDefinitions()
-    getAllDefinitions()
+    await Promise.all([
+      getDatasetDefinitions(),
+      getAllDefinitions(),
+    ])
   }
 }
 
 async function unassignDefinition(id: Number) {
   const response = await axios.post("/definitions/unassign", { definitionId: id, datasetName: datasetName }, { validateStatus: null });
   if (response.status == HttpStatusCode.Ok) {
-    getDatasetDefinitions()
-    getAllDefinitions()
+    await Promise.all([
+      getDatasetDefinitions(),
+      getAllDefinitions(),
+    ])
   }
 }
 
@@ -78,7 +84,7 @@ if (response.status == 200) {
     <div class="flex mt-2">
       <div class=" bg-slate-400 rounded-xl m-2 p-2 w-8/12">
         <h1 class="font-bold">Definitions assigned to this dataset:</h1>
-        <listElement @remove="unassignDefinition(definition.id)" v-for="definition in datasetDefinitions" :data="definition" />
+        <ListElement @remove="unassignDefinition(definition.id)" v-for="definition in datasetDefinitions" :data="definition" />
       </div>
       <div class="bg-slate-400 rounded-xl m-2 p-2 w-1/3">
         <div class="flex m-1">
